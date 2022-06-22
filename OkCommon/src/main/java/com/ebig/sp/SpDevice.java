499b6b4ca4892262abb1a8bfd8cda91fba16c2bd
wwl <2429314223@qq.com>
2022-06-22 09:11:10 +0800

    spdevice 序列号码

--- OkCommon/src/main/java/com/ebig/sp/SpDevice.java
+++ OkCommon/src/main/java/com/ebig/sp/SpDevice.java
@@ -4,6 +4,8 @@ public class SpDevice {
 
     private static final String TENANT_ID = "TENANT_ID";/*租户id*/
     public static final String FactoryCode = "FactoryCodeIns";/*设备序列码*/
+    public static final String serialNumber = "serialNumber";/*序列号*/
+
 
 
     public static void save(String code) {
@@ -12,6 +14,14 @@ public class SpDevice {
 
     public static String getCode() {
         return BaseSp.l().getString(FactoryCode);
+    }
+
+    public static void saveSerialNumber(String code) {
+        BaseSp.l().putString(FactoryCode, code);
+    }
+
+    public static String getSerialNumber() {
+        return BaseSp.l().getString(serialNumber);
     }
 
     /*设置租户id*/
